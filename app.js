5a493a9a50dc8a737d805ad96dab20d929a7b22e
Nakuljod <113530964+Nakuljod@users.noreply.github.com>
2023-03-26 02:08:09 +0530

    math operation in js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 console.log("oyeee")
+
+var num = 5 // in line comment
+
+/* multi line 
+
+comment */
+
+
+// data types
+// 1. undefined
+// 2. null
+// 3. boolean 
+// 4. objects
+
+// declare a keyword there are 3 methods
+
+// var , let , const
+
+
+// var num = 3;
+
+// let sum = 4;
+
+// const abc = 9;
+
+
+
+
+
+
+
+// mathamatical operations in js
+
+// var add = 7 + 8;
+// var diff = 5 - 5;
+// var multi = 7 * 8;
+// var div = 8 % 2;
+
+
+
